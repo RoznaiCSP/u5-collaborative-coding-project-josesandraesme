eb7436fb9298acc23935c6668589947e4d9735a1
Jggonzalez15 <120490139+Jggonzalez15@users.noreply.github.com>
2023-01-27 09:41:04 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+function doSomething3 () {
+    projectile3 = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . 3 3 3 3 3 3 3 3 . . . . 
+        . . . 3 d 3 3 3 3 3 3 c 3 . . . 
+        . . 3 c d 3 3 3 3 3 3 c c 3 . . 
+        . 3 c c d d d d d d 3 c c d 3 d 
+        . 3 c 3 a a a a a a a b c d 3 3 
+        . 3 3 a b b a b b b a a b d 3 3 
+        . 3 a b b b a b b b b a 3 3 3 3 
+        . a a 3 3 3 a 3 3 3 3 3 a 3 3 3 
+        . a a a a a a f a a a f a 3 d d 
+        . a a a a a a f a a f a a a 3 d 
+        . a a a a a a f f f a a a a a a 
+        . a f f f f a a a a f f f a a a 
+        . . f f f f f a a f f f f f a . 
+        . . . f f f . . . . f f f f . . 
+        . . . . . . . . . . . . . . . . 
+        `, 50, 0)
+    projectile3.setPosition(5, 68)
+    projectile3.setFlag(SpriteFlag.DestroyOnWall, true)
+    pause(1500)
+}
+// This functions loads projectiles of red car
 function doSomething () {
     projectile = sprites.createProjectileFromSide(img`
         . . . . . . . . . . . . . . . . 
@@ -18,11 +42,59 @@ function doSomething () {
         . . . . . . . . . . . . . . . . 
         `, 50, 0)
     projectile.setPosition(5, 15)
-    mySprite.setFlag(SpriteFlag.AutoDestroy, true)
-    pause(2000)
+    projectile.setFlag(SpriteFlag.DestroyOnWall, true)
+    pause(1000)
 }
+function doSomething2 () {
+    projectile2 = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . 6 6 6 6 6 6 6 6 . . . . 
+        . . . 6 9 6 6 6 6 6 6 c 6 . . . 
+        . . 6 c 9 6 6 6 6 6 6 c c 6 . . 
+        . 6 c c 9 9 9 9 9 9 6 c c 9 6 d 
+        . 6 c 6 8 8 8 8 8 8 8 b c 9 6 6 
+        . 6 6 8 b b 8 b b b 8 8 b 9 6 6 
+        . 6 8 b b b 8 b b b b 8 6 6 6 6 
+        . 8 8 6 6 6 8 6 6 6 6 6 8 6 6 6 
+        . 8 8 8 8 8 8 f 8 8 8 f 8 6 d d 
+        . 8 8 8 8 8 8 f 8 8 f 8 8 8 6 d 
+        . 8 8 8 8 8 8 f f f 8 8 8 8 8 8 
+        . 8 f f f f 8 8 8 8 f f f 8 8 8 
+        . . f f f f f 8 8 f f f f f 8 . 
+        . . . f f f . . . . f f f f . . 
+        . . . . . . . . . . . . . . . . 
+        `, 50, 0)
+    projectile2.setPosition(5, 35)
+    projectile2.setFlag(SpriteFlag.DestroyOnWall, true)
+    pause(1000)
+}
+function doSomething4 () {
+    projectile4 = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . 5 5 5 5 5 5 5 5 . . . . 
+        . . . 5 4 5 5 5 5 5 5 c 5 . . . 
+        . . 5 c 4 5 5 5 5 5 5 c c 5 . . 
+        . 5 c c 4 4 4 4 4 4 5 c c 4 5 d 
+        . 5 c 5 5 5 5 5 5 5 5 b c 4 5 5 
+        . 5 5 5 b b 5 b b b 5 5 b 4 5 5 
+        . 5 5 b b b 5 b b b b 5 5 5 5 5 
+        . 5 5 4 4 4 5 4 4 4 4 4 5 5 5 5 
+        . 5 5 5 5 5 5 f 5 5 5 f 5 5 d d 
+        . 5 5 5 5 5 5 f 5 5 f 5 5 5 5 d 
+        . 5 5 5 5 5 5 f f f 5 5 5 5 5 5 
+        . 5 f f f f 5 5 5 5 f f f 5 5 5 
+        . . f f f f f 5 5 f f f f f 5 . 
+        . . . f f f . . . . f f f f . . 
+        . . . . . . . . . . . . . . . . 
+        `, 50, 0)
+    projectile4.setPosition(5, 95)
+    projectile4.setFlag(SpriteFlag.DestroyOnWall, true)
+    pause(1000)
+}
+let projectile4: Sprite = null
+let projectile2: Sprite = null
 let projectile: Sprite = null
-let mySprite: Sprite = null
+let projectile3: Sprite = null
 scene.setBackgroundImage(img`
     7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
     7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
@@ -145,7 +217,7 @@ scene.setBackgroundImage(img`
     7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
     7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
     `)
-mySprite = sprites.create(img`
+let mySprite = sprites.create(img`
     . . . . . . . . . . 2 2 2 . . . 
     . . . . . . . . . . 2 2 2 . . . 
     . . . . . . . . . 1 1 1 1 1 . . 
@@ -167,5 +239,8 @@ mySprite.setPosition(74, 111)
 mySprite.setStayInScreen(true)
 controller.moveSprite(mySprite)
 for (let index = 0; index < 7; index++) {
+    doSomething4()
+    doSomething3()
+    doSomething2()
     doSomething()
 }

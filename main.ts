421ae62546834b9498827626ec1728ccd3412bca
Jggonzalez15 <120490139+Jggonzalez15@users.noreply.github.com>
2023-01-29 23:42:47 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,6 +18,10 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     game.gameOver(false)
     music.play(music.melodyPlayable(music.powerDown), music.PlaybackMode.InBackground)
 })
+let car4: Sprite = null
+let car3: Sprite = null
+let car2: Sprite = null
+let car: Sprite = null
 let mySprite: Sprite = null
 tiles.setCurrentTilemap(tilemap`level1`)
 mySprite = sprites.create(img`
@@ -41,5 +45,87 @@ mySprite = sprites.create(img`
 mySprite.setPosition(75, 380)
 scene.cameraFollowSprite(mySprite)
 game.onUpdateInterval(750, function () {
-	
+    car = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . 3 3 3 3 3 3 3 3 . . . . . . 
+        . 3 d 3 3 3 3 3 3 c 3 . . . . . 
+        3 c d 3 3 3 3 3 3 c c 3 . . . . 
+        c c d d d d d d 3 c c d 3 d . . 
+        c 3 a a a a a a a b c d 3 3 d . 
+        3 a b b a b b b a a b d 3 3 3 . 
+        a b b b a b b b b a 3 3 3 3 3 . 
+        a 3 3 3 a 3 3 3 3 3 a 3 3 3 3 . 
+        a a a a a f a a a f a 3 d d 3 . 
+        a a a a a f a a f a a a 3 d d . 
+        a a a a a f f f a a a a a a a . 
+        f f f f a a a a f f f a a a a . 
+        f f f f f a a f f f f f a . . . 
+        . f f f . . . . f f f f . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    car.setVelocity(50, 0)
+    tiles.placeOnRandomTile(car, assets.tile`myTile`)
+    car.setFlag(SpriteFlag.DestroyOnWall, true)
+    car2 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . 6 6 6 6 6 6 6 6 . . . . 
+        . . . 6 9 6 6 6 6 6 6 c 6 . . . 
+        . . 6 c 9 6 6 6 6 6 6 c c 6 . . 
+        . 6 c c 9 9 9 9 9 9 6 c c 9 6 d 
+        . 6 c 6 8 8 8 8 8 8 8 b c 9 6 6 
+        . 6 6 8 b b 8 b b b 8 8 b 9 6 6 
+        . 6 8 b b b 8 b b b b 8 6 6 6 6 
+        . 8 8 6 6 6 8 6 6 6 6 6 8 6 6 6 
+        . 8 8 8 8 8 8 f 8 8 8 f 8 6 d d 
+        . 8 8 8 8 8 8 f 8 8 f 8 8 8 6 d 
+        . 8 8 8 8 8 8 f f f 8 8 8 8 8 8 
+        . 8 f f f f 8 8 8 8 f f f 8 8 8 
+        . . f f f f f 8 8 f f f f f 8 . 
+        . . . f f f . . . . f f f f . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    car2.setVelocity(50, 0)
+    tiles.placeOnRandomTile(car2, assets.tile`myTile0`)
+    car3 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . 2 2 2 2 2 2 2 2 . . . . 
+        . . . 2 4 2 2 2 2 2 2 c 2 . . . 
+        . . 2 c 4 2 2 2 2 2 2 c c 2 . . 
+        . 2 c c 4 4 4 4 4 4 2 c c 4 2 d 
+        . 2 c 2 e e e e e e e b c 4 2 2 
+        . 2 2 e b b e b b b e e b 4 2 2 
+        . 2 e b b b e b b b b e 2 2 2 2 
+        . e e 2 2 2 e 2 2 2 2 2 e 2 2 2 
+        . e e e e e e f e e e f e 2 d d 
+        . e e e e e e f e e f e e e 2 d 
+        . e e e e e e f f f e e e e e e 
+        . e f f f f e e e e f f f e e e 
+        . . f f f f f e e f f f f f e . 
+        . . . f f f . . . . f f f f . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    car3.setVelocity(50, 0)
+    tiles.placeOnRandomTile(car3, assets.tile`myTile4`)
+    car3.setFlag(SpriteFlag.DestroyOnWall, true)
+    car4 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . 2 2 2 2 2 2 2 2 . . . . 
+        . . . 2 4 2 2 2 2 2 2 c 2 . . . 
+        . . 2 c 4 2 2 2 2 2 2 c c 2 . . 
+        . 2 c c 4 4 4 4 4 4 2 c c 4 2 d 
+        . 2 c 2 e e e e e e e b c 4 2 2 
+        . 2 2 e b b e b b b e e b 4 2 2 
+        . 2 e b b b e b b b b e 2 2 2 2 
+        . e e 2 2 2 e 2 2 2 2 2 e 2 2 2 
+        . e e e e e e f e e e f e 2 d d 
+        . e e e e e e f e e f e e e 2 d 
+        . e e e e e e f f f e e e e e e 
+        . e f f f f e e e e f f f e e e 
+        . . f f f f f e e f f f f f e . 
+        . . . f f f . . . . f f f f . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    car4.setVelocity(50, 0)
+    tiles.placeOnRandomTile(car4, assets.tile`myTile3`)
+    car4.setFlag(SpriteFlag.DestroyOnWall, true)
 })

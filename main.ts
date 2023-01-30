eca038338792f7e9e3068d11aa51b81312fb98db
Jggonzalez15 <120490139+Jggonzalez15@users.noreply.github.com>
2023-01-29 18:43:22 -0600

    Restore '1/29/2023, 6:39:28 PM Update main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -61,6 +61,9 @@ function doSomething () {
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.y += 16
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileDarkGrass2, function (sprite, location) {
+    game.gameOver(true)
+})
 function doSomething2 () {
     projectile2 = sprites.createProjectileFromSide(img`
         . . . . . . . . . . . . . . . . 
